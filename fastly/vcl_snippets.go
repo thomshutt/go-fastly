b9af164f831debc6001606c38739e4bab0164c92
Thom Shutt <thomshutt@users.noreply.github.com>
2018-01-08 11:53:20 +0000

    Added ability to list VCL snippets

---
+++ fastly/vcl_snippets.go
@@ -0,0 +1,101 @@
+package fastly
+
+import (
+	"fmt"
+	"sort"
+	"time"
+)
+
+// VCLSnippetType defines the location in generated VCL where the snippet should be placed.
+type VCLSnippetType string
+
+const (
+	// Place the snippet above all subroutines.
+	VCLSnippetTypeInit VCLSnippetType = "init"
+
+	// Place the snippet within the vcl_recv subroutine but below the boilerplate VCL and above any objects.
+	VCLSnippetTypeRecv VCLSnippetType = "recv"
+
+	// Place the snippet within the vcl_hit subroutine.
+	VCLSnippetTypeHit VCLSnippetType = "hit"
+
+	// Place the snippet within the vcl_miss subroutine.
+	VCLSnippetTypeMiss VCLSnippetType = "miss"
+
+	// Place the snippet within the vcl_pass subroutine.
+	VCLSnippetTypePass VCLSnippetType = "pass"
+
+	// Place the snippet within the vcl_fetch subroutine.
+	VCLSnippetTypeFetch VCLSnippetType = "fetch"
+
+	// Place the snippet within the vcl_error subroutine.
+	VCLSnippetTypeError VCLSnippetType = "error"
+
+	// Place the snippet within the vcl_deliver subroutine.
+	VCLSnippetTypeDeliver VCLSnippetType = "deliver"
+
+	// Place the snippet within the vcl_log subroutine.
+	VCLSnippetTypeLog VCLSnippetType = "log"
+
+	// Don't render the snippet in VCL so it can be manually included in custom VCL.
+	VCLSnippetTypeNone VCLSnippetType = "none"
+)
+
+// VCLSnippet represents a VCL Snippet response from the Fastly API.
+type VCLSnippet struct {
+	ServiceID string `mapstructure:"service_id"`
+	Version   int    `mapstructure:"version"`
+
+	Content   string         `mapstructure:"content"`
+	Dynamic   bool           `mapstructure:"dynamic"`
+	ID        string         `mapstructure:"id"`
+	Name      string         `mapstructure:"name"`
+	Priority  uint           `mapstructure:"priority"`
+	Type      VCLSnippetType `mapstructure:"type"`
+	CreatedAt *time.Time     `mapstructure:"created_at"`
+	UpdatedAt *time.Time     `mapstructure:"updated_at"`
+	DeletedAt *time.Time     `mapstructure:"deleted_at"`
+}
+
+// snippetsByName is a sortable list of VCL Snippets.
+type snippetsByName []*VCLSnippet
+
+// Len, Swap, and Less implement the sortable interface.
+func (s snippetsByName) Len() int      { return len(s) }
+func (s snippetsByName) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s snippetsByName) Less(i, j int) bool {
+	return s[i].Name < s[j].Name
+}
+
+// ListVCLSnippetsInput is used as input to the ListVCLSnippets function.
+type ListVCLSnippetsInput struct {
+	// Service is the ID of the service (required).
+	Service string
+
+	// Version is the specific configuration version (required).
+	Version int
+}
+
+// ListVCLSnippets returns the list of VCL Snippets for the configuration version.
+func (c *Client) ListVCLSnippets(i *ListVCLSnippetsInput) ([]*VCLSnippet, error) {
+	if i.Service == "" {
+		return nil, ErrMissingService
+	}
+
+	if i.Version == 0 {
+		return nil, ErrMissingVersion
+	}
+
+	path := fmt.Sprintf("/service/%s/version/%d/snippet", i.Service, i.Version)
+	resp, err := c.Get(path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	var bs []*VCLSnippet
+	if err := decodeJSON(&bs, resp.Body); err != nil {
+		return nil, err
+	}
+	sort.Stable(snippetsByName(bs))
+	return bs, nil
+}
